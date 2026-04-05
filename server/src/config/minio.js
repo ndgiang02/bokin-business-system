85f6437948a2db0c455a33a71b487ce447c9d817
Nguyen Duy Giang <giang.duyng@gmail.com>
2026-04-06 00:50:52 +0700

    fix minio...

--- server/src/config/minio.js
+++ server/src/config/minio.js
@@ -4,8 +4,8 @@ export const minioClient = new Minio.Client({
   endPoint:  process.env.MINIO_ENDPOINT  || 'minio',
   port:      parseInt(process.env.MINIO_PORT || '9000'),
   useSSL:    process.env.MINIO_USE_SSL === 'true',
-  accessKey: process.env.MINIO_ACCESS_KEY || 'root',
-  secretKey: process.env.MINIO_SECRET_KEY || '12345678',
+  accessKey: process.env.MINIO_ROOT_USER || 'root',
+  secretKey: process.env.MINIO_ROOT_PASSWORD || '12345678',
 });
 
 export const BUCKET = process.env.MINIO_BUCKET || 'business-system';
